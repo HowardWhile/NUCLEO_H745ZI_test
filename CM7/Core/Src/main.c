33dcf6f05bda08590724fe0d9124f2b5ca0d6bd2
HowardCheng <makubex49@gmail.com>
2020-04-20 10:36:18 +0800

    核心分別控制不同的LD 忽略編譯產物的資料夾

--- CM7/Core/Src/main.c
+++ CM7/Core/Src/main.c
@@ -165,6 +165,8 @@ Error_Handler();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	  HAL_Delay(500);
   }
   /* USER CODE END 3 */
 }
